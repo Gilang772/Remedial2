578fc0e5d55a5fe8acbc03b6d71b2bedb3b307f7
Gilang772 <gilang2016langsa@gmail.com>
2021-05-30 14:44:43 +0700

    ubah login

--- appem/index.php
+++ appem/index.php
@@ -13,7 +13,7 @@
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900, s900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -33,42 +33,35 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              <div class="col-lg-12 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-12">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">selamat datang!</h1><h3>Di Aplikasi Pengaduan Masyarakat</h3>   
                   </div>
-                  <form class="user">
+                  <form class="user" method="post" action="cek_login.php>">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="text" name="username" class="form-control form-control-user" aria-describedby="emailHelp" placeholder="masukan username">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      <input type="password" name ="password" class="form-control form-control-user" placeholder="password">
                     </div>
                     <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
+                       
                     </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
+                    <input type="submit"  class="btn btn-primary btn-user btn-block" value="login">
+                      
                     </a>
                     <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
+                   
                   </form>
+                  <div class="text-center">
+                    Belum punya akun?<br>
+                    <a class="small" href="register.php">register?</a>
                   <hr>
+        
                   <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
+                    <a class="small" href="">Masuk sebagai Admin/Petugas</a>
                   </div>
                 </div>
               </div>
